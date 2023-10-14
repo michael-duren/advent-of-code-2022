5929d1963d993eaa933fc65adc99648d3623c22f
Michael Duren <michaeld@michaelduren.com>
2023-10-14 07:02:17 -0500

    finished solution for day-1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -25,30 +25,30 @@ fn determine_calories() -> u64 {
 }
 
 fn determine_top_three_elves() -> u64 {
-    let file = std::fs::read_to_string("puzzle_input").unwrap();
-    let mut all_carries: Vec<Vec<u64>> = vec![];
-    let mut current_carry: Vec<u64> = vec![]; // current elf determination before a new line occurs
+    let file = std::fs::read_to_string("puzzle_input").unwrap(); // read file, returning Ok value
+    let mut all_carries: Vec<Vec<u64>> = vec![]; // create a vector of vectors to store each elves carry
+    let mut current_carry: Vec<u64> = vec![]; // current elf carry before a new line occurs
 
     for line in file.lines() {
         match line.is_empty() {
             true => {
-                all_carries.push(current_carry);
-                current_carry = vec![];
+                all_carries.push(current_carry); // push current carry to all carries
+                current_carry = vec![]; // reset current carry, all_carries has ownership of previous value
             }
             false => {
-                let parsed_num: u64 = line.parse().unwrap();
-                current_carry.push(parsed_num);
+                let parsed_num: u64 = line.parse().unwrap(); // parse line to u64
+                current_carry.push(parsed_num); // push parsed number to current carry
             }
         }
     }
 
-    // println!(":?", all_carries);
+    if !current_carry.is_empty() {
+        all_carries.push(current_carry);
+    } // push last carry to all carries
 
     let mut summed_calories: Vec<u64> = all_carries.iter().map(|c| c.iter().sum()).collect();
     summed_calories.sort_by(|a, b| b.cmp(a));
-    let top_3_summed: u64 = summed_calories[(summed_calories.len() - 4)..summed_calories.len()]
-        .iter()
-        .sum();
+    let top_three_summed: u64 = summed_calories.iter().take(3).sum();
 
-    return top_3_summed;
+    return top_three_summed;
 }
