6245829171aa0aba93371ed0bfe7057fa3629f05
Michael Duren <michaeld@michaelduren.com>
2023-10-26 19:46:40 -0500

    finish day-3 part-1

---
+++ day-3/notes.md
@@ -0,0 +1,7 @@
+Each rucksack has 2 large compartments.
+All items of a given type are expected to go into exactly one of the 2 compartments.
+First half of line represents items in 1st compartment, 2nd half is 2nd compartment
+
+Every Item type can be converted to a **priority**
+- Lowercase item types `a` through `z` have priorities 1-26
+- Uppercase item types `A` through `Z` have priorities 27 through 52
--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,1 +1,37 @@
-fn main() {}
+use std::collections::HashMap;
+
+fn get_point_dic() -> HashMap<char, i32> {
+    let mut point_dict: HashMap<char, i32> = HashMap::new();
+
+    for (index, char) in ('a'..='z').enumerate() {
+        point_dict.insert(char, index as i32 + 1);
+    }
+
+    for (index, char) in ('A'..='Z').enumerate() {
+        point_dict.insert(char, index as i32 + 27);
+    }
+
+    return point_dict;
+}
+
+fn main() {
+    let lines = include_str!("../input").lines();
+    let point_dict = get_point_dic();
+    let mut total_points: i32 = 0;
+
+    for line in lines {
+        // TODO: Split the lines in half
+        let first_half = &line[..line.len() / 2];
+        let second_half = &line[line.len() / 2..];
+
+        for char in first_half.chars() {
+            if second_half.contains(char) {
+                let point = point_dict.get(&char);
+                total_points += point.unwrap();
+                break;
+            }
+        }
+    }
+
+    println!("Total Points: {}", total_points);
+}
