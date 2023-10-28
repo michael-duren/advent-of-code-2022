16454f196ae64f6ef4c6232ad23206bbf4bea4f1
Michael Duren <michaeld@michaelduren.com>
2023-10-28 17:53:20 -0500

    added error handling

--- day-3-part-2/src/main.rs
+++ day-3-part-2/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error};
 
 fn get_point_dic() -> HashMap<char, i32> {
     let mut point_dict: HashMap<char, i32> = HashMap::new();
@@ -15,31 +15,46 @@ fn get_point_dic() -> HashMap<char, i32> {
 }
 
 // find the unique letter in three different lines
-fn find_badge(lines: Vec<&str>) -> Option<char> {
-    for c in lines.get(0).unwrap().chars() {
-        if lines.get(1).unwrap().contains(c) && lines.get(2).unwrap().contains(c) {
-            return Some(c);
+fn find_badge(lines: Vec<&str>) -> Result<Option<char>, &'static str> {
+    if lines.len() != 3 {
+        return Err("Invalid number of lines");
+    }
+
+    for c in lines[0].chars() {
+        if lines[1].contains(c) && lines[2].contains(c) {
+            return Ok(Some(c));
         }
     }
 
-    return None;
+    return Ok(None);
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let lines: Vec<&str> = include_str!("../input").lines().collect();
     let point_dict = get_point_dic();
     let mut total_points: i32 = 0;
 
     for i in 0..lines.len() {
         if i % 3 == 0 || i == 0 {
+            // check for out of bounds
             if i + 2 < lines.len() {
                 let three_lines = vec![lines[i], lines[i + 1], lines[i + 2]];
                 let badge = find_badge(three_lines);
-                println!("Badge: {:?}", badge);
-                total_points += point_dict.get(&badge.unwrap()).unwrap();
+                match badge {
+                    Ok(Some(c)) => {
+                        let found_points =
+                            point_dict.get(&c).ok_or("Invalid letter for points dict")?;
+                        total_points += found_points;
+                    }
+                    Ok(None) => panic!("Error: There was no badge found. Iteration: {}", i),
+                    Err(e) => {
+                        panic!("Error: There was an issue finding a badge: {}", e);
+                    }
+                }
             }
         }
     }
 
     println!("Total points: {}", total_points);
+    return Ok(());
 }
